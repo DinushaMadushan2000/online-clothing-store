c0df18140233b4e4739215f128b910c93be993c2
Dinusha Madushan <madaduw-se20042@stu.kln.ac.lk>
2024-08-04 21:43:15 +0530

    Add configeration part

---
+++ Online-Clothing-Store/src/main/java/com/zosh/config/JwtConstant.java
@@ -0,0 +1,6 @@
+package com.zosh.config;
+
+public class JwtConstant {
+    public static final String SECRET_KEY="vjjbsdvdgsjwbdhcgyegsbdhsgsjsjshdhd";
+    public static final String JWT_HEADER="Authurization";
+}
